91e8be41f114c7c1012a3fba398df9f6dd2bd13b
thyGamerr <32223644+thyGamerr@users.noreply.github.com>
2025-10-23 13:24:14 -0700

    Update service-worker.js
    trying to fix

--- service-worker.js
+++ service-worker.js
@@ -9,19 +9,16 @@ self.addEventListener('install', e=>{
 });
 
 self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e=>{
-  const url = new URL(e.request.url);
   if (e.request.method !== 'GET') return;
   e.respondWith(
     caches.match(e.request).then(cached=>{
       const fetchPromise = fetch(e.request).then(res=>{
-        const copy = res.clone();
-        caches.open(CACHE).then(c=>c.put(e.request, copy));
+        caches.open(CACHE).then(c=>c.put(e.request, res.clone()));
         return res;
       }).catch(()=>cached || Response.error());
       return cached || fetchPromise;
