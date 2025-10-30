f72ba1e787ee5a4cedc913e6e34960c6b7960a19
thyGamerr <32223644+thyGamerr@users.noreply.github.com>
2025-10-30 13:07:05 -0700

    Update and rename config.js to auth-config.js
    fixing microsoft login

--- auth-config.js
+++ auth-config.js
@@ -1,4 +1,3 @@
-// config.js
 window.APP_CONFIG = {
   appName: "Drone LogBook",
   version: "1.0.0",
@@ -14,15 +13,10 @@ window.AUTH_CONFIG = {
 
   // --- MICROSOFT LOGIN ---
   microsoft: {
-    // From your Azure App Registration → Overview
     clientId: "54598063-7dcf-4a6e-9340-5537c1974dc4", // Application (client) ID
     tenantId: "553965ce-fc07-4db9-b229-9040c2431e77", // Directory (tenant) ID
     authority: "https://login.microsoftonline.com/553965ce-fc07-4db9-b229-9040c2431e77",
-
-    // Must match one of your registered redirect URIs in Azure (with or without slash)
     redirectUri: "https://thygamerr.github.io/Drone-Logbook/",
-
-    // Delegated scopes you approved in Azure
     scopes: [
       "openid",
       "profile",
